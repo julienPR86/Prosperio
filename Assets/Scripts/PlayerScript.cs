acfa21fcfb81453e0d6829b4da51316474662011
JulienPR86 <julien.pr86@gmail.com>
2024-12-05 14:58:11 +0100

    add player prefab + script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,69 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/*public class PlayerScript : MonoBehaviour
+{
+    private InputSystem_Actions _inputs;
+    private InputAction _move;
+    private InputAction _mouse;
+    Vector2 movementVector = new(0,0);
+    private int _borderSize = 50;
+    private int _navigationSpeed = 1;
+
+    void Awake()
+    {
+        _inputs = new InputSystem_Actions();
+    }
+    void OnEnable()
+    {
+        _inputs.Enable();
+        _move = _inputs.Player.Move;
+        _mouse = _inputs.Player.Mouse;
+    }
+    void OnDisable()
+    {
+        _inputs.Disable();
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector2 mousePosition = _mouse.ReadValue<Vector2>();
+        movementVector = _move.ReadValue<Vector2>();
+        movementVector += MouseMovement(mousePosition);
+        transform.position += new Vector3(movementVector.x*_navigationSpeed*Time.deltaTime, 0, movementVector.y*_navigationSpeed*Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Based on the mouse position, return a Vector2
+    /// </summary>
+    /// <param name="mousePosition">The mouse position</param>
+    /// <returns>Return a right, left, up or down Vector2</returns>
+    private Vector2 MouseMovement(Vector2 mousePosition)
+    {
+        Vector2 movement = new(0,0);
+        if (mousePosition.x < 0 + _borderSize)
+        {
+            movement.Set(-1, movement.y);
+        }
+        else if (mousePosition.x > Screen.width - _borderSize )
+        {
+            movement.Set(1, movement.y);
+        }
+        if (mousePosition.y < 0 + _borderSize)
+        {
+            movement.Set(movement.x, -1);
+        }
+        else if (mousePosition.y > Screen.height - _borderSize)
+        {
+            movement.Set(movement.x, 1);
+        }
+        return movement;
+    }
+}
+*/
