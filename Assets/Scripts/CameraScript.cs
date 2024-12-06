5b3813d2ed923e7272501959a9d5c377fb1232c3
JulienPR86 <julien.pr86@gmail.com>
2024-12-06 10:56:17 +0100

    passed the camera to orthographic and setup limits

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraScript : MonoBehaviour
 {
+    private Camera _camera;
     private InputController _inputs;
     private InputAction _move;
     private InputAction _mouse;
     Vector2 movementVector = new(0,0);
     private int _borderSize = 50;
     private int _navigationSpeed = 1;
-
-    private int _x_limit = 1920;
-    private int _y_limit = 1080;
+    private float _cameraSize;
+    private Vector2 _limitOffset;
+    private Vector2 _limits = new(1920, 1080);
 
     void Awake()
     {
@@ -31,7 +33,11 @@ public class CameraScript : MonoBehaviour
     }
     void Start()
     {
-
+        _camera = GetComponent<Camera>();
+        _cameraSize = _camera.orthographicSize;
+        float y =_cameraSize;
+        float x = _cameraSize*Screen.width/Screen.height;
+        _limitOffset.Set(x, y);
     }
 
     void Update()
@@ -40,22 +46,22 @@ public class CameraScript : MonoBehaviour
         movementVector = _move.ReadValue<Vector2>();
         movementVector += SideMovement(_mouse.ReadValue<Vector2>());
         transform.position += new Vector3(movementVector.x*_navigationSpeed*Time.deltaTime, movementVector.y*_navigationSpeed*Time.deltaTime, 0);
-        //check if the camera is out of the map
-        if (transform.position.x < 0)
+        //check if the camera is out of the map based on the map and camera size
+        if (transform.position.x < 0 + _limitOffset.x)
         {
-            transform.position = new(0, transform.position.y, 0);
+            transform.position = new(_limitOffset.x, transform.position.y, transform.position.z);
         }
-        else if (transform.position.x > _x_limit)
+        else if (transform.position.x > _limits.x - _limitOffset.x)
         {
-            transform.position = new(_x_limit, transform.position.y, 0);
+            transform.position = new(_limits.x - _limitOffset.x, transform.position.y, transform.position.z);
         }
-        if (transform.position.y < 0)
+        if (transform.position.y < 0 + _limitOffset.y)
         {
-            transform.position = new(transform.position.x, 0, 0);
+            transform.position = new(transform.position.x, _limitOffset.y, transform.position.z);
         }
-        else if (transform.position.y > _y_limit)
+        else if (transform.position.y > _limits.y - _limitOffset.y)
         {
-            transform.position = new(transform.position.x, _y_limit, 0);
+            transform.position = new(transform.position.x, _limits.y - _limitOffset.y, transform.position.z);
         }
     }
 
