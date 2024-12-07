01e0477fece40e05816b29567f076d483370027b
RoyalHugo <hugo.imperat@gmail.com>
2024-12-07 19:31:47 +0100

    Population Move System

--- Assets/Scripts/ManageClock.cs
+++ Assets/Scripts/ManageClock.cs
@@ -8,7 +8,7 @@ public class ManageClock : MonoBehaviour
     private int second = 0;
     public int day = 1;
     private float elapsedTime = 0f;
-    private float speed = 10000;
+    private float speed = 100;
     public TextMeshProUGUI textClock;
 
     void Start()
