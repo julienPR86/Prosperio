a4c0a59e3d14e3091a16b6a08b59d3e2ed97e862
RoyalHugo <hugo.imperat@gmail.com>
2024-12-07 19:31:47 +0100

    Population Move System

--- Assets/Scripts/Population.cs
+++ Assets/Scripts/Population.cs
@@ -11,19 +11,15 @@ public class Population : MonoBehaviour
     private GridManager gridManager;
     private ManageClock clock;
     private bool isWorking = false;
-    private Dictionary<GameObject, Person> personDictionary = new Dictionary<GameObject, Person>();
     void Start()
     {
         gridManager = GetComponent<GridManager>();
         clock = GetComponent<ManageClock>();
-
-        // Beginning: 2 wanderers + 1 harvester + 1 lumberjack + 1 digger + 1 mason
-        CreateGameObject(Job.Wanderer);
-        CreateGameObject(Job.Wanderer);
-        CreateGameObject(Job.Harvester);
-        CreateGameObject(Job.Lumberjack);
-        CreateGameObject(Job.Digger);
-        CreateGameObject(Job.Mason);
+        Person wanderer = new Person();
+        Person harvester = new Person(Job.Harvester);
+        Person lumberjack = new Person(Job.Lumberjack);
+        Person digger = new Person(Job.Digger);
+        Person mason = new Person(Job.Mason);
     }
 
     private void Update()
@@ -33,7 +29,6 @@ public class Population : MonoBehaviour
             GoToWork(Job.Harvester);
             GoToWork(Job.Lumberjack);
             GoToWork(Job.Digger);
-            StartWalkingAround();
             isWorking = true;
         }
         if (clock.GetTime() == 0 && isWorking) 
@@ -53,38 +48,30 @@ public class Population : MonoBehaviour
         personobject.transform.localScale = new Vector3(0.3f, 0.3f);
         SpriteRenderer spriteRenderer = personobject.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = circleSprite;
-        Person person = null;
 
         switch (job) 
         {
             case Job.Harvester:
                 spriteRenderer.color = new Color(9f / 255f, 166f / 255f, 3f / 255f);
                 personobject.transform.SetParent(stock.transform.Find("Harvesters"));
-                person = new Person(Job.Harvester);
                 break;
             case Job.Wanderer:
                 spriteRenderer.color = new Color(139f / 255f, 140f / 255f, 139f / 255f);
                 personobject.transform.SetParent(stock.transform.Find("Wanderers"));
-                person = new Person();
                 break;
             case Job.Lumberjack:
                 spriteRenderer.color = new Color(133f / 255f, 75f / 255f, 36f / 255f);
                 personobject.transform.SetParent(stock.transform.Find("Lumberjacks"));
-                person = new Person(Job.Lumberjack);
                 break;
             case Job.Digger:
                 spriteRenderer.color = new Color(255f / 255f, 255f / 255f, 255f / 255f);
                 personobject.transform.SetParent(stock.transform.Find("Diggers"));
-                person = new Person(Job.Digger);
                 break;
             case Job.Mason:
                 spriteRenderer.color = new Color(252f / 255f, 130f / 255f, 0f / 255f);
                 personobject.transform.SetParent(stock.transform.Find("Masons"));
-                person = new Person(Job.Mason);
                 break;
         }
-
-        personDictionary.Add(personobject, person);
     }
 
     private void GoToWork(Job job)
@@ -136,7 +123,7 @@ public class Population : MonoBehaviour
 
     public void GoHere(GameObject o, Vector3 location)
     {
-        float speed = 3f;
+        float speed = 5f;
         StartCoroutine(MoveToTarget(o, location, speed));
     }
 
@@ -157,39 +144,7 @@ public class Population : MonoBehaviour
         o.transform.position = location;
     }
 
-    private void StartWalkingAround()
-    {
-        Transform wanderersFolder = stock.transform.Find("Wanderers").transform;
-        for (int i = 0; i < wanderersFolder.childCount; i++)
-        {
-            GameObject wanderer = wanderersFolder.GetChild(i).gameObject;
-            StartCoroutine(WalkRandomly(wanderer));
-        }
-    }
 
-    private IEnumerator WalkRandomly(GameObject wanderer) // Coroutine to make the wanderers walking around
-    {
-        while (true)
-        {
-            // Randomly pick a location
-            int randomX = Random.Range(0, gridManager.width);
-            int randomY = Random.Range(0, gridManager.height);
-
-            Vector3 randomLocation = gridManager.cells[randomX, randomY].WorldPosition;
-
-            // Move the wanderer to the new random location
-            yield return MoveToTarget(wanderer, randomLocation, 3f); // Adjust speed as needed
-
-            float waitTime = Random.Range(1f, 3f); // Repeat after waiting X seconds
-            yield return new WaitForSeconds(waitTime);
-        }
-    }
-
-
-    public void GoToSleep()
-    {
-        // function called at the end of the day to make people go to sleep
-    }
 
 
 }
